fdded94662582793eff1f8e74e24cd980ec0fbdb
Jagadeesh Kumar Natesan <jag.psg@gmail.com>
2019-04-26 00:18:45 +0530

    Project Manager Service - Initial Check-in

---
+++ src/main/java/com/project/manager/ProjectManagerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.project.manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectManagerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectManagerServiceApplication.class, args);
+	}
+
+}
