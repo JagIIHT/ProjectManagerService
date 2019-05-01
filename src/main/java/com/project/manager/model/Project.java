086d8985e1c58fa4348d243d47c23f7b828ae50a
Jagadeesh Kumar Natesan <jag.psg@gmail.com>
2019-05-01 09:13:42 +0530

    Entity creation and Repository check-in

---
+++ src/main/java/com/project/manager/model/Project.java
@@ -0,0 +1,88 @@
+package com.project.manager.model;
+
+import java.time.LocalDate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Transient;
+
+@Entity
+public class Project {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "Project_ID")
+	private long projectId;
+	@Column(name = "Project")
+	private String name;
+	@Column(name = "Priority")
+	private int priority;
+	@Column(name = "Start_Date")
+	private LocalDate startDate;
+	@Column(name = "End_Date")
+	private LocalDate endDate;
+	@Transient
+	private String status;
+	@Transient
+	private User user;
+
+	public long getProjectId() {
+		return projectId;
+	}
+
+	public void setProjectId(long projectId) {
+		this.projectId = projectId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getPriority() {
+		return priority;
+	}
+
+	public void setPriority(int priority) {
+		this.priority = priority;
+	}
+
+	public LocalDate getStartDate() {
+		return startDate;
+	}
+
+	public void setStartDate(LocalDate startDate) {
+		this.startDate = startDate;
+	}
+
+	public LocalDate getEndDate() {
+		return endDate;
+	}
+
+	public void setEndDate(LocalDate endDate) {
+		this.endDate = endDate;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+}
