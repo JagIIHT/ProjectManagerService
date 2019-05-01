7761aa35731b08e3f5488546dc27fb522eb443ee
Jagadeesh Kumar Natesan <jag.psg@gmail.com>
2019-05-01 09:17:05 +0530

    Repository correction

--- src/main/java/com/project/manager/repository/ProjectRepository.java
+++ src/main/java/com/project/manager/repository/ProjectRepository.java
@@ -1,5 +1,11 @@
 package com.project.manager.repository;
 
-public class ProjectRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.project.manager.model.Project;
+
+@Repository
+public interface ProjectRepository extends JpaRepository<Project, Long>{
 
 }
--- src/main/java/com/project/manager/repository/UserRepository.java
+++ src/main/java/com/project/manager/repository/UserRepository.java
@@ -1,5 +1,11 @@
 package com.project.manager.repository;
 
-public class UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.project.manager.model.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
 
 }
